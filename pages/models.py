8dfd89efa584efc274360cc8d871c767ce7828ce
rejath-chandran <71695833+rejath-chandran@users.noreply.github.com>
2021-03-01 23:03:14 +0530

    media field created

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+class Team(models.Model):
+    first_name = models.CharField(max_length=225)
+    last_name = models.CharField(max_length=255)
+    designation = models.CharField(max_length=225)
+    photo = models.ImageField(upload_to='photos/%Y/%M/%D/')
+    facebook_link = models.URLField(max_length=100)
+    twitter_link = models.URLField(max_length=100)
+    created_date = models.DateField(auto_now_add=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.first_name
+
+     
